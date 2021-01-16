4360a79a6606acd65d838a4a6a5581f9149c000a
Mike Urbanski <mike@bridgecrew.io>
2021-01-16 09:56:54 -0600

    move internal constants to locals

--- locals.tf
+++ locals.tf
@@ -1,10 +1,22 @@
 locals {
-  queue_name          = "${var.customer_name}-RemediationsDLQ.fifo"
-  inbound_queue_name  = "${var.customer_name}-InboundRemediations.fifo"
-  account_id          = data.aws_caller_identity.current.account_id
-  sns_topic_arn       = "arn:aws:sns:${local.region}:${var.organizationID}:${var.snsNotifyTopic}"
-  region              = data.aws_region.current.name
-  role_name           = "${var.customer_name}-RemediationFunction"
-  queue_principal_arn = "arn:aws:iam::${var.organizationID}:root"
-  function_name       = "BridgecrewRemoteRemediation"
+  # Bridgecrew account to receive notifications
+  organization_id                    = "890234264427"
+  sns_notify_topic                   = "handle-customer-actions"
+  queue_name                         = "${var.customer_name}-RemediationsDLQ.fifo"
+  inbound_queue_name                 = "${var.customer_name}-InboundRemediations.fifo"
+  account_id                         = data.aws_caller_identity.current.account_id
+  sns_topic_arn                      = "arn:aws:sns:${local.region}:${local.organization_id}:${local.sns_notify_topic}"
+  region                             = data.aws_region.current.name
+  role_name                          = "${var.customer_name}-RemediationFunction"
+  queue_principal_arn                = "arn:aws:iam::${local.organization_id}:root"
+  function_name                      = "BridgecrewRemoteRemediation"
+  fifo_queue                         = true
+  template_bucket                    = "bc-code-artifacts-${local.organization_id}-"
+  lambda_zip_name                    = "prod/remediations_lambda_c5f16a2212411fd69a5c6a5fe37278617df82f5a.zip"
+  outbound_remediations_endpoint     = "https://dfak3u9wq1.execute-api.us-west-2.amazonaws.com/v1"
+  template_version                   = "0.3.37"
+  batch_size                         = 1
+  runtime                            = "nodejs10.x"
+  lambda_timeout                     = 900
+  maximum_batching_window_in_seconds = 0
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "kms_data_key_reuse_period_seconds" {
   type    = number
+  description = "The length of time that SQS may use a cached data key before calling KMS again. Lower this value to force more frequent KMS check-ins."
   default = 300
 }
 
@@ -9,73 +10,14 @@ variable "common_tags" {
   default     = {}
 }
 
-variable "fifo_queue" {
-  description = "Is this queue fifo?"
-  type        = bool
-  default     = true
-}
-
 variable "api-token" {
   type        = string
   description = "Bridgecrew Platform API key"
   sensitive   = true
 }
 
-variable "organizationID" {
-  type    = string
-  default = "890234264427"
-}
-
-variable "templateBucket" {
-  type        = string
-  description = "Public bucket to get the Lambda zipped code from"
-  default     = "bc-code-artifacts-890234264427-"
-}
-
-variable "lambdaZipName" {
-  type        = string
-  description = "The Object to get from the Bucket"
-  default     = "prod/remediations_lambda_c5f16a2212411fd69a5c6a5fe37278617df82f5a.zip"
-}
-
-variable "outboundRemediationsEndpoint" {
-  type    = string
-  default = "https://dfak3u9wq1.execute-api.us-west-2.amazonaws.com/v1"
-}
-
-variable "snsNotifyTopic" {
-  type    = string
-  default = "handle-customer-actions"
-}
-
-variable "templateVersion" {
-  type    = string
-  default = "0.3.37"
-}
-
-variable "batch_size" {
-  type    = number
-  default = 1
-}
-
-variable "maximum_batching_window_in_seconds" {
-  type    = number
-  default = 0
-}
-
-variable "runtime" {
-  description = "Set the runtime for the Remediations Lambda"
-  type        = string
-  default     = "nodejs10.x"
-}
-
 variable "customer_name" {
   type        = string
-  description = "Customer name identifier - e.g. Patreon, Bridgecrew"
+  description = "Bridgecrew organization name that you created during account setup."
 }
 
-variable "Lambda_timeout" {
-  description = "Lambda execution timeout"
-  type        = number
-  default     = 900
-}
